8b835765cc3c0897310bfeeb7bcc97138bbed489
en-coded <83879346+en-coded@users.noreply.github.com>
2021-05-08 14:45:13 -0400

    Updated till task 1.3

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Task 1.1
+
+const mainEl = document.querySelector('main');
+
+// Task 1.2
+
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+
+//Task 1.3
+
+mainEl.classList.add('flex-ctr');
